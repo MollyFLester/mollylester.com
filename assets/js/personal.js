ca4cee090b38dc7efc11d944d778b70480e640ad
Molly Lester <picklepets16@Molly-Lesters-MacBook-Pro.local>
2015-12-28 20:17:30 -0600

    Landing Page_v01
    Changes to the color, captions, logo

---
+++ assets/js/personal.js
@@ -0,0 +1,12 @@
+/* basic script to change picture on click, need to make it functional
+for more than one image, and also have a nice transition */
+
+function changeImage(){
+	var image = document.getElementById('changedImage');
+	if(image.src.match("af_diagram")){
+		image.src = "http://placehold.it/980x500";
+	} else{
+		image.src = "assets/images/learning/af_diagram.jpg";
+	}
+
+};
